1353d5308bc33bde01ebab8cb846cc57078b7984
Jeremy C <jchow@Jeremys-MacBook-Pro.local>
2019-10-15 17:41:08 -0700

    Updates

--- precache-manifest.f7181aea884c5aa0b88bedd7044c69ef.js
+++ precache-manifest.f7181aea884c5aa0b88bedd7044c69ef.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0c6e494abe51c35f5694fc8c2a9e503",
+    "revision": "ee4aafdbc32ea5aa01adf2d6279b4d49",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "eb1eaa554a2389b505e2",
+    "revision": "80baafe758fb69300e7c",
     "url": "/portfolio/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "caf2ba98d147119f34c5",
+    "revision": "4686bf5aeaa6bf251e0f",
     "url": "/portfolio/static/css/main.4940457b.chunk.css"
   },
   {
-    "revision": "eb1eaa554a2389b505e2",
-    "url": "/portfolio/static/js/2.e9201e5b.chunk.js"
+    "revision": "80baafe758fb69300e7c",
+    "url": "/portfolio/static/js/2.f368f9ea.chunk.js"
   },
   {
-    "revision": "caf2ba98d147119f34c5",
-    "url": "/portfolio/static/js/main.4e0a4c83.chunk.js"
+    "revision": "4686bf5aeaa6bf251e0f",
+    "url": "/portfolio/static/js/main.a0cc1dbf.chunk.js"
   },
   {
     "revision": "c5306228cc83f6f6271c",
